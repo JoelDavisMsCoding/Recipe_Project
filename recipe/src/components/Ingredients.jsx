c902f9f11c37f07057c020a3c8ea310756a91b5a
JoelDavisMsCoding <166549396+JoelDavisMsCoding@users.noreply.github.com>
2025-07-14 21:20:16 -0500

    adding .env file

--- recipe/src/components/Ingredients.jsx
+++ recipe/src/components/Ingredients.jsx
@@ -9,7 +9,7 @@ function Recipes() {
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
-        const response = await fetch('http://localhost:3000/api');
+        const response = await fetch(`${import.meta.env.VITE_API_URL}`);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
